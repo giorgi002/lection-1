ea5b3bd56dec225db919bb5ea480993b88557334
giorgi002 <167451976+giorgi002@users.noreply.github.com>
2024-09-23 21:41:41 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+
+// const nums = [ 2,3,4,5,6,7,8,10];
+
+
+// const filtered = nums.filter(item => item % 2 ==1);
+// console.log(filtered);
+
+
+
+// const strs = ["hello", "nika", "gela", "asddd", "a",];
+// function getcharacters
+
+
+
+// function removeLastCharacter(strings) {
+//     return strings.map(str => str.slice(0, -1));
+// }
+
+// const arrayOfStrings = ['hello', 'world', 'example', 'test'];
+// const result = removeLastCharacter(arrayOfStrings);
+// console.log(result);
+
+
+const numbers = [ 1,32,5,76,242,21,43]
+
+function sumOfLowest(string){
+    const sorted = string.sort((a, b) => a -b);
+    return sorted[0] + sorted [1]
+}
+console.log(sumOfLowest(numbers));
+
+
